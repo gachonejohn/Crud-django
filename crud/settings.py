731dd8cf9951080d11b98fbc47f5fc78bb388be0
John Thuku <ceogachone@gmail.com>
2024-11-23 19:12:36 +0300

    Third commit

--- crud/settings.py
+++ crud/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'crud_app',  
         'USER':'root',  
-        'PASSWORD':'Forex#254',  
+        'PASSWORD':'',  
         'HOST':'localhost',  
         'PORT':'3306'  
     }  
